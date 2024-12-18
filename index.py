dc70f9366ceb7abe2e4ddae2f24696fc1c3fbb08
BouchraBoumedien <bb891975@ens.uqam.ca>
2024-12-17 19:12:41 -0500

    tout

--- index.py
+++ index.py
@@ -15,7 +15,11 @@
 from flask import Flask
 from flask import render_template
 from flask import g
-from database import Database
+from flask import jsonify
+import random
+
+from requests import request
+from .database import Database
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
@@ -34,7 +38,75 @@ def close_connection(exception):
         db.disconnect()
 
 
+# Route principale
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route pour le formulaire de mise en adoption
+@app.route('/form')
 def form():
-    # À remplacer par le contenu de votre choix.
     return render_template('form.html')
+
+# Route pour naviguer la liste d'animaux disponibles
+@app.route('/adopter')
+def liste():
+    db = get_db()
+    animals = db.get_animaux()
+    return render_template('adopter.html', animals=animals)
+
+# Route pour effectuer des recherches et leurs resultats
+@app.route('/search')
+def search():
+    query = request.args.get('query', '').lower()
+    db = get_db()
+    animals = db.get_animaux()
+
+    results = [
+        animal for animal in animals
+        if query in ' '.join(str(value).lower() for value in animal.values())
+    ]
+
+    return render_template('resultat_recherches.html', animals=results)
+
+#Route pour consulter le profil d'un animal
+@app.route('/animal/<int:animal_id>')
+def animal_detail(animal_id):
+    db = get_db()
+    animal = db.get_animal(animal_id)
+    if animal:
+        return render_template('profil-animal.html', animal=animal)
+    return "Animal pas trouvé", 404
+
+#Route pour l'affichage aleratoire dans la page d'acceuil
+@app.route('/api/random-animals')
+def random_animals():
+    db = get_db()
+    animals = db.get_animaux()
+    random_animals = random.sample(animals, 5)
+    return jsonify(random_animals)
+
+#Route pour l'ajout d'un animal
+@app.route('/api/add-animal', methods=['POST'])
+def add_animal():
+    data = request.json
+    db = get_db()
+    try:
+        animal_id = db.add_animal(
+            data['nom'],
+            data['espece'],
+            data['race'],
+            data['age'],
+            data['description'],
+            data['courriel'],
+            data['adresse'],
+            data['ville'],
+            data['cp']
+        )
+        return jsonify({'success': True, 'animal_id': animal_id}), 201
+    except Exception as e:
+        print(f"Erreur lors d'ajout d'animal: {e}")
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
